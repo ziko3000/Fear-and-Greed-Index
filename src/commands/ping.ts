192e9f0ad08b574fcd621680df4e69ed8ab5033d
ziko3000 <fammercury@gmail.com>
2023-07-17 16:01:06 +0700

    Final

--- src/commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import { CommandInteraction } from 'discord.js';
-
-export class PingCommand {
-  async execute(interaction: CommandInteraction): Promise<void> {
-    await interaction.reply('Pong!');
-  }
-}
